76b87af02f0bbf0de8935cd6bdacc72d0e482d44
yangjun85824 <yangjun85824@163.com>
2022-12-09 10:51:24 +0800

    no message

---
+++ yyld-light4j/src/main/java/com/networknt/petstore/handler/PetsGetHandler.java
@@ -0,0 +1,33 @@
+package com.networknt.petstore.handler;
+
+import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
+
+import com.networknt.handler.LightHttpHandler;
+import com.networknt.http.HttpMethod;
+import com.networknt.http.HttpStatus;
+import com.networknt.http.MediaType;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+import io.undertow.util.HeaderMap;
+
+import java.util.Deque;
+import java.util.Map;
+
+/**
+For more information on how to write business handlers, please check the link below.
+https://doc.networknt.com/development/business-handler/rest/
+*/
+public class PetsGetHandler implements LightHttpHandler {
+
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        // HeaderMap requestHeaders = exchange.getRequestHeaders();
+        // Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
+        String responseBody = "[{\"id\":1,\"name\":\"catten\",\"tag\":\"cat\"},{\"id\":2,\"name\":\"doggy\",\"tag\":\"dog\"}]";
+        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        exchange.setStatusCode(HttpStatus.OK.value());
+        exchange.getResponseSender().send(responseBody);
+    }
+}
---
+++ yyld-light4j/src/main/java/com/networknt/petstore/handler/PetsPostHandler.java
@@ -0,0 +1,35 @@
+package com.networknt.petstore.handler;
+
+import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
+
+import com.networknt.handler.LightHttpHandler;
+import com.networknt.http.HttpMethod;
+import com.networknt.http.HttpStatus;
+import com.networknt.http.MediaType;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+import io.undertow.util.HeaderMap;
+import com.networknt.petstore.model.Pet;
+import java.util.Deque;
+import java.util.Map;
+
+/**
+For more information on how to write business handlers, please check the link below.
+https://doc.networknt.com/development/business-handler/rest/
+*/
+public class PetsPostHandler implements LightHttpHandler {
+
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        // HeaderMap requestHeaders = exchange.getRequestHeaders();
+        // Map<String, Deque<String>> queryParameters = exchange.getQueryParameters();
+        Map<String, Object> bodyMap = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+        Pet requestBody = Config.getInstance().getMapper().convertValue(bodyMap, Pet.class);
+        String responseBody = "{}";
+        exchange.getResponseHeaders().add(Headers.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        exchange.setStatusCode(HttpStatus.OK.value());
+        exchange.getResponseSender().send(responseBody);
+    }
+}
